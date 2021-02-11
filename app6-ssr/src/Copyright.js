21c3836897d040f7d759cbf938bee2c3f06a9efd
Sabrina-AP <72463853+Sabrina-AP@users.noreply.github.com>
2021-02-11 18:07:18 -0300

    ssr - Aula AVA - Recode Pro

--- app6-ssr/src/Copyright.js
+++ app6-ssr/src/Copyright.js
@@ -2,6 +2,14 @@
 import React from 'react';
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props => {
-    return <h1>Recode Pro {props.ano}!</h1>;
+    return (
+        <div>
+            <h1 className='col text-white text-center bg-primary mt-5'>Recode Pro {props.ano}! </h1>
+            <br /><br /><br />
+            <p className='col text-white text-center bg-success p-5 '>A Sabrina A-G-R-A-D-E-C-E imensamente a oportunidade incrível de aprendizado!!!!!!!!!!!*_* :)</p>
+            <br /><br />
+            <p className='text-dark col text-white text-center bg-warning p-5'>*_* :)!!!!!!!!!!!!!! ********* MUITO OBRIGADA ********* !!!!!!!!!!!!!!*_* :)</p>
+        </div>
+    )
 };
 
